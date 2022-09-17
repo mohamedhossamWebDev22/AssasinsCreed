01527a696056ed1d9ecc69f4796d763bbc64dee8
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2022-09-17 10:48:07 +0200

    Add games and profile cards

--- js/app.js
+++ js/app.js
@@ -1,21 +1,40 @@
 let navBar = document.getElementById('nav'),
-tobBtn = document.getElementById('top');
+profileDOM = document.getElementById('profile'),
+topBtn = document.getElementById('top');
 
 let navOffest = navBar.offsetTop;
 
 function navColor(){
-    if (window.pageYOffset >= navOffest){
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        topBtn.style.display = "block";
         navBar.style.background = 'rgb(0, 110, 255)';
-    }
-    else{
+    } 
+    else {
+        topBtn.style.display = "none";
         navBar.style.background = 'rgb(0, 110, 255, 0)';
     }
 }
-function topBtn(){
-    tobBtn.style.display = 'block';
+
+function profile() {
+    if (localStorage.length == 0){
+        let name = prompt('Enter your name:');
+        
+        localStorage.setItem('UserName', name);
+            
+        var nme = localStorage.getItem('UserName');
+
+        profileDOM.innerHTML = `Hi ${nme} assassin`;
+    }
+    else{
+        var nme = localStorage.getItem('UserName');
+
+        profileDOM.innerHTML = `Hi ${nme} assassino`;
+    }
 }
 
+window.onload = function (){
+    profile();
+}
 window.onscroll = function (){
     navColor();
-    topBtn();
 }
